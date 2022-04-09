ce3f615a9ccdc6aed4322bd63f57798fa6fe17fa
Andrey Larikov <larikov174@gmail.com>
2022-04-10 00:46:33 +0500

    feat: add main scene init

---
+++ src/components/MainScene.js
@@ -0,0 +1,47 @@
+import Phaser from 'phaser';
+import FONT_PROPS from '../utils/utils';
+
+export default class MainScene extends Phaser.Scene {
+  constructor() {
+    super('MainScene');
+  }
+
+  init() {
+    this.startX = 45;
+    this.startY = 230;
+    this.endX = 590;
+    this.endY = 690;
+    this.gameWidth = this.sys.game.config.width;
+    this.gameHeigth = this.sys.game.config.height;
+    this.sceneZone = this.add.zone(this.gameWidth / 2, this.gameHeigth / 2, this.gameWidth, this.gameHeigth);
+    this.frames = ['blue', 'purple', 'red', 'yellow', 'green'];
+    this.grid = [];
+    this.connected = [];
+    this.chosenColor = null;
+    this.highscore = localStorage.highscore ? JSON.parse(localStorage.highscore) : 0;
+    this.score = 0;
+    this.goal = 5;
+    this.moves = 10;
+    this.possibleMoves = [];
+    this.level = 1;
+    this.levelText = '';
+    this.movesText = '';
+    this.scoreText = '';
+    this.highscoreText = '';
+    this.progressBar = this.add.graphics({ fillStyle: { color: 0x199d21 } });
+    this.progressOverlay = this.add.graphics({ fillStyle: { color: 0x001a3e } });
+    this.rectBar = new Phaser.Geom.Rectangle(250, 45, 0, 30);
+    this.rectOverlay = new Phaser.Geom.Rectangle(250, 45, 420, 30);
+    this.camera = this.cameras.add();
+  }
+
+  preload() {
+
+  }
+
+  create() {
+
+  }
+
+
+}
--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import Preloader from '../components/Preloader';
+import MainScene from '../components/MainScene';
 
 const config = {
 	type: Phaser.AUTO,
@@ -7,7 +8,7 @@ const config = {
 	width: 990,
 	height: 820,
 	backgroundColor: '#a1a1a1',
-	scene: [Preloader],
+	scene: [Preloader, MainScene],
 };
 
 export default config;
