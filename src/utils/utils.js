e56ecfb513c242b71debab28bef0990f1b2d0f7e
Andrey Larikov <larikov174@gmail.com>
2022-04-10 00:38:30 +0500

    feat: add font props

---
+++ src/utils/utils.js
@@ -0,0 +1,12 @@
+const FONT_PROPS = (text, size = 24) => (
+  {
+    text: text,
+    style: {
+      fontSize: `${size}px`,
+      fontFamily: 'Calibri',
+      color: '#fff',
+    }
+  }
+);
+
+export default FONT_PROPS;
