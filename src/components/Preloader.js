8cc5babc25ba1c55795888a00f9743c9c21bca52
Andrey Larikov <larikov174@gmail.com>
2022-04-21 02:08:16 +0500

    fix: preloader code cleanUp

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -27,17 +27,18 @@ export default class Preloader extends Phaser.Scene {
     const { SIZE_L, SIZE_S } = FONT_PROPS.SIZE;
     const width = this.sys.game.config.width;
     const height = this.sys.game.config.height;
+    const screenCenter = this.add.zone(width / 2, height / 2, width, height);
     const title = this.createLabel(0, 0, MAIN_TITLE, SIZE_L);
     const text = this.createLabel(0, 0, BEGIN_TEXT, SIZE_S);
     const cubeR = this.add.image(0, 0, 'sprites', 'red').setScale(1.5);
     const cubeB = this.add.image(0, 0, 'sprites', 'blue').setScale(1.5);
     const cubeG = this.add.image(0, 0, 'sprites', 'green').setScale(2.5);
 
-    Phaser.Display.Align.In.TopCenter(title, this.add.zone(width / 2, height - 220, width, height));
-    Phaser.Display.Align.In.Center(cubeR, this.add.zone(width / 2 - 115, height / 2, width, height));
-    Phaser.Display.Align.In.Center(cubeB, this.add.zone(width / 2 + 115, height / 2, width, height));
-    Phaser.Display.Align.In.Center(cubeG, this.add.zone(width / 2, height / 2, width, height));
-    Phaser.Display.Align.In.BottomCenter(text, this.add.zone(width / 2, height - 620, width, height));
+    Phaser.Display.Align.In.QuickSet(title, screenCenter, 6, 0, -150);
+    Phaser.Display.Align.In.QuickSet(text, screenCenter, 6, 0, 150);
+    Phaser.Display.Align.In.QuickSet(cubeR, screenCenter, 6, -115, 0);
+    Phaser.Display.Align.In.QuickSet(cubeB, screenCenter, 6, 115, 0);
+    Phaser.Display.Align.In.QuickSet(cubeG, screenCenter, 6, 0, 0);
 
     this.input.once('pointerdown', () => {
       this.scene.start('MainScene');
