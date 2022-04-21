4b9dbea8e60860365efd9eead2a7d4a84486589f
Andrey Larikov <larikov174@gmail.com>
2022-04-21 09:52:58 +0500

    fix: gameOver component code cleanUp

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
-import FONT_PROPS from '../utils/utils';
+import LabelCreator from '../ui/LabelCreator';
+import FONT_PROPS from '../ui/FontProps';
 import CONST from '../utils/constants';
 
 export default class GameOver extends Phaser.Scene {
@@ -11,7 +12,16 @@ export default class GameOver extends Phaser.Scene {
     this.cameras.main.fadeIn(250);
   }
 
-  create() {
+  createLabel(x, y, value, size) {
+    const { FAMILY, FILL } = FONT_PROPS;
+    const style = { fontSize: `${size}px`, fill: FILL, fontFamily: FAMILY };
+    const label = new LabelCreator(this, x, y, value, style);
+    this.add.existing(label);
+    label.depth = 3;
+    return label;
+  }
+
+  createCubefall() {
     this.arr = [];
     for (let i = 0; i < 20; i++) {
       const color = Phaser.Math.Between(0, 4);
@@ -20,22 +30,33 @@ export default class GameOver extends Phaser.Scene {
       let block = this.add.sprite(sx, sy, 'sprites', CONST.FRAMES[color]).setScale(0.5).setAlpha(0.7);
       this.arr[i] = block;
     }
+  }
 
+  create() {
+    this.createCubefall();
+    const { AGAIN, CHEERUP, NEW_RECORD, NEW_SCORE, BEST_SCORE } = CONST.TEXT;
+    const { SIZE_L, SIZE_S } = FONT_PROPS.SIZE;
     const width = this.sys.game.config.width;
     const height = this.sys.game.config.height;
-    const textAgain = this.make.text(FONT_PROPS(CONST.TEXT.AGAIN));
-    const title = this.make.text(
-      FONT_PROPS(this.registry.get('new') == true ? CONST.TEXT.NEW_RECORD : CONST.TEXT.CHEERUP, 48),
-    );
-    const textScore = this.make.text(
-      FONT_PROPS(
-        `${this.registry.get('new') == true ? CONST.TEXT.NEW_SCORE : CONST.TEXT.BEST_SCORE} ${localStorage.highscore}`,
-      ),
-    );
+    const screenCenter = this.add.zone(width / 2, height / 2, width, height);
+    const content = () => {
+      if (this.registry.get('new')) return {
+        text1: NEW_RECORD,
+        text2: `${NEW_SCORE} ${localStorage.highscore}`
+      };
+      else return {
+        text1: CHEERUP,
+        text2: `${BEST_SCORE} ${localStorage.highscore}`
+      };
+    };
 
-    Phaser.Display.Align.In.Center(title, this.add.zone(width / 2, height / 2, width, height));
-    Phaser.Display.Align.In.Center(textScore, this.add.zone(width / 2, height / 2 + 50, width, height));
-    Phaser.Display.Align.In.Center(textAgain, this.add.zone(width / 2, height / 2 + 300, width, height));
+    const textAgain = this.createLabel(0, 0, AGAIN, SIZE_S);
+    const title = this.createLabel(0, 0, content().text1, SIZE_L);
+    const textScore = this.createLabel(0, 0, content().text2, SIZE_S);
+
+    Phaser.Display.Align.In.QuickSet(title, screenCenter, 6, 0, 0);
+    Phaser.Display.Align.In.QuickSet(textScore, screenCenter, 6, 0, 50);
+    Phaser.Display.Align.In.QuickSet(textAgain, screenCenter, 6, 0, 300);
 
     this.input.once('pointerdown', () => {
       this.scene.start('MainScene');
