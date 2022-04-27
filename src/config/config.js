a9fd388a22347041b14c42e4f221bfbf84dd7f8b
Andrey Larikov <larikov174@gmail.com>
2022-04-27 10:31:50 +0500

    feat: add desktop layout

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@ const { GAME_WIDTH, GAME_HEIGHT, GAME_BACKGROUND } = CONST;
 
 const config = {
   type: Phaser.AUTO,
-  parent: 'body',
+  parent: 'phaser-game',
   width: GAME_WIDTH,
   height: GAME_HEIGHT,
   backgroundColor: GAME_BACKGROUND,
@@ -28,16 +28,23 @@ window.onload = function() {
 
 function resize() {
   const canvas = document.querySelector("canvas");
+  const body = document.querySelector("body");
+  const footer = document.querySelector(".footer");
+  console.log(footer.getBoundingClientRect().height);
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
   const windowRatio = windowWidth / windowHeight;
   const gameRatio = GAME_WIDTH /GAME_HEIGHT;
   if(windowRatio < gameRatio) {
-      canvas.style.width = windowWidth + "px";
-      canvas.style.height = (windowWidth / gameRatio) + "px";
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = (windowWidth / gameRatio) + "px";
+    body.style.height = (windowWidth / gameRatio) - 150 + "px";
+    footer.style.width = windowWidth + "px"
   } else {
-      canvas.style.width = (windowHeight * gameRatio) + "px";
-      canvas.style.height = windowHeight + "px";
+    canvas.style.width = (windowHeight * gameRatio) + "px";
+    canvas.style.height = windowHeight - 150 + "px";
+    body.style.height = (windowWidth * gameRatio) - 150 + "px";
+    footer.style.width = (windowHeight * gameRatio) + "px";
   }
 }
 
