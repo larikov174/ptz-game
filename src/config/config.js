b19c21afddf12a40d1db9e0aa158a68210bae0af
Andrey Larikov <larikov174@gmail.com>
2022-04-30 15:37:45 +0500

    feat: v3

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,11 @@ import MainScene from '../components/MainScene';
 import GameOver from '../components/GameOver';
 import CONST from '../utils/constants';
 
-const { GAME_WIDTH, GAME_HEIGHT, GAME_BACKGROUND } = CONST;
+const {
+  GAME_WIDTH,
+  GAME_HEIGHT,
+  GAME_BACKGROUND
+} = CONST;
 
 const config = {
   type: Phaser.AUTO,
@@ -13,38 +17,38 @@ const config = {
   height: GAME_HEIGHT,
   backgroundColor: GAME_BACKGROUND,
   scene: [Preloader, MainScene, GameOver],
-  // scale: {
-  //   parent: 'phaser-game',
-  //   mode: Phaser.Scale.ScaleModes.RESIZE,
-  //   width: GAME_WIDTH,
-  //   height: GAME_HEIGHT,
-  // },
+  scale: {
+    parent: 'phaser-game',
+    mode: Phaser.Scale.ScaleModes.FIT,
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
+  },
 };
 
-window.onload = function() {
-  resize();
-  window.addEventListener("resize", resize);
-};
-
-function resize() {
-  const canvas = document.querySelector("canvas");
-  const body = document.querySelector("body");
-  const footer = document.querySelector(".footer");
-  const windowWidth = window.innerWidth;
-  const windowHeight = window.innerHeight;
-  const windowRatio = windowWidth / windowHeight;
-  const gameRatio = GAME_WIDTH /GAME_HEIGHT;
-  if(windowRatio < gameRatio) {
-    canvas.style.width = `${windowWidth - (windowWidth > 500 ? 30 : 0)}px`;
-    canvas.style.height = (windowWidth / gameRatio) + "px";
-    body.style.height = (windowWidth / gameRatio) - 150 + "px";
-    footer.style.width = `${windowWidth - (windowWidth > 500 ? 30 : 0)}px`;
-  } else {
-    canvas.style.width = (windowHeight * gameRatio)  + "px";
-    canvas.style.height = windowHeight - 150 + "px";
-    body.style.height = (windowWidth * gameRatio) - 150 + "px";
-    footer.style.width = (windowHeight * gameRatio) + "px";
-  }
-}
+// function resize() {
+//   const canvas = document.querySelector("canvas");
+//   const body = document.querySelector("body");
+//   const footer = document.querySelector(".footer");
+//   const windowWidth = window.innerWidth;
+//   const windowHeight = window.innerHeight;
+//   const windowRatio = windowWidth / windowHeight;
+//   const gameRatio = GAME_WIDTH / GAME_HEIGHT;
+//   if (windowRatio < gameRatio) {
+//     canvas.style.width = `${windowWidth - (windowWidth > 500 ? 30 : 0)}px`;
+//     canvas.style.height = `${windowWidth / gameRatio  }px`;
+//     body.style.height = `${(windowWidth / gameRatio) - 150  }px`;
+//     footer.style.width = `${windowWidth - (windowWidth > 500 ? 30 : 0)}px`;
+//   } else {
+//     canvas.style.width = `${windowHeight * gameRatio   }px`;
+//     canvas.style.height = `${windowHeight - 150  }px`;
+//     body.style.height = `${(windowWidth * gameRatio) - 150  }px`;
+//     footer.style.width = `${windowHeight * gameRatio  }px`;
+//   }
+// }
+//
+// window.onload = () => {
+//   resize();
+//   window.addEventListener("resize", resize);
+// };
 
 export default config;
