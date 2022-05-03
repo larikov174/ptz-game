e978d4d8ddd838b7e57e3576e0c509e0346f0e59
Andrey Larikov <larikov174@gmail.com>
2022-05-04 01:44:14 +0500

    fix: remove anchor link from url after page reload

--- src/utils/handler.js
+++ src/utils/handler.js
@@ -3,9 +3,16 @@ const mainBlock = document.querySelector('.info');
 const container = document.querySelector('.container');
 const footer = document.querySelector('.footer');
 
+window.location.replace("#");
+
+if (typeof window.history.replaceState == 'function') {
+  history.replaceState({}, '', window.location.href.slice(0, -1));
+}
+
+window.screenY = 0;
 
 button.addEventListener('click', () => {
   mainBlock.classList.add('idle');
   footer.classList.add('idle');
   container.classList.remove('idle');
-})
+});
