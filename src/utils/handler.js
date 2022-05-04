5717490be698ea8e878e02f633830192d661c1b9
Andrey Larikov <larikov174@gmail.com>
2022-05-04 11:19:10 +0500

    feat: add burger menu

--- src/utils/handler.js
+++ src/utils/handler.js
@@ -1,9 +1,12 @@
-const button = document.querySelector('.info__button');
+const startGameButton = document.querySelector('.info__button');
 const mainBlock = document.querySelector('.info');
-const container = document.querySelector('.container');
+const gameCanvas = document.querySelector('.container');
 const footer = document.querySelector('.footer');
+const burgerButton = document.querySelector('.header__button');
+const burgerMenu = document.querySelector('.burger-menu');
 
-window.location.replace("#");
+
+window.location.replace('#');
 
 if (typeof window.history.replaceState == 'function') {
   history.replaceState({}, '', window.location.href.slice(0, -1));
@@ -11,8 +14,18 @@ if (typeof window.history.replaceState == 'function') {
 
 window.screenY = 0;
 
-button.addEventListener('click', () => {
+startGameButton.addEventListener('click', () => {
   mainBlock.classList.add('idle');
   footer.classList.add('idle');
-  container.classList.remove('idle');
+  gameCanvas.classList.remove('idle');
+});
+
+burgerButton.addEventListener('click', () => {
+  burgerMenu.classList.toggle('idle');
+  burgerButton.classList.toggle('header__button_close');
+});
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.add('idle');
+  burgerButton.classList.remove('header__button_close');
 });
