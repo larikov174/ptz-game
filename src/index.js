0ff08699288d25ab59221c96820df509d78f606d
Andrey Larikov <larikov174@gmail.com>
2022-05-04 11:51:47 +0500

    fix: code cleanUp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import Phaser from 'phaser';
 import config from './config/config';
-import handler from './utils/handler'
+import handler from './handler'
 
 const game = new Phaser.Game(config);
