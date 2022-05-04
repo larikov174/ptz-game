aa97b858a5b5c8c1722c31d7729917e8ed608038
Andrey Larikov <larikov174@gmail.com>
2022-05-05 01:59:55 +0500

    fix: code cleanUp

--- src/components/MainScene.js
+++ src/components/MainScene.js
@@ -4,7 +4,7 @@ import SFX from '../ui/SFX';
 import LabelCreator from '../ui/LabelCreator';
 import CONST from '../utils/constants';
 
-const { HIGHSCORE, SCORE, TIME, CUBE_HEIGHT, START_Y, GAME_WIDTH, GAME_HEIGHT, OVERLAY_COLOR } = CONST;
+const { HIGHSCORE, SCORE, TIME, CUBE_HEIGHT, START_Y, GAME_WIDTH } = CONST;
 const { FAMILY, SIZE_XL, SIZE_M, FC_WHITE } = CONST.FONT_PROPS;
 
 export default class MainScene extends Phaser.Scene {
