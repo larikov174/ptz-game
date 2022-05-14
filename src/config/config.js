463adb5c8ad0c0fe9e8e1e993cd7d03d6319ec62
Andrey Larikov <larikov174@gmail.com>
2022-05-14 15:39:02 +0500

    fix: tuneUp game config

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,10 @@
 import Phaser from 'phaser';
 import Preloader from '../components/Preloader';
-import {MainScene} from '../components/MainScene';
+import { MainScene } from '../components/MainScene';
 import GameOver from '../components/GameOver';
 import CONST from '../utils/constants';
 
-const {
-  GAME_WIDTH,
-  GAME_HEIGHT,
-  GAME_BACKGROUND
-} = CONST;
+const { GAME_WIDTH, GAME_HEIGHT, GAME_BACKGROUND } = CONST;
 
 const config = {
   type: Phaser.AUTO,
@@ -17,29 +13,37 @@ const config = {
   height: GAME_HEIGHT,
   backgroundColor: GAME_BACKGROUND,
   scene: [Preloader, MainScene, GameOver],
+  title: 'КОНКУРС - ФПК - ЧЕТРА',
+  url: 'https://fpktech.ru',
+  version: '3.0',
+  banner: {
+    hidePhaser: true,
+    text: '#fff',
+    background: ['#4b2942', '#f0af1d', '#fff', '#04336a']
+  },
 };
 
 function resize() {
-  const canvas = document.querySelector("#phaser-game");
-  const body = document.querySelector("body");
+  const canvas = document.querySelector('#phaser-game');
+  const body = document.querySelector('body');
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
   const windowRatio = windowWidth / windowHeight;
   const gameRatio = GAME_WIDTH / GAME_HEIGHT;
   if (windowRatio < gameRatio) {
     canvas.style.width = `${windowWidth - (windowWidth > 500 ? 30 : 0)}px`;
-    canvas.style.height = `${windowWidth / gameRatio  }px`;
-    body.style.height = `${(windowWidth / gameRatio)  }px`;
+    canvas.style.height = `${windowWidth / gameRatio}px`;
+    body.style.height = `${windowWidth / gameRatio}px`;
   } else {
-    canvas.style.width = `${windowHeight * gameRatio   }px`;
-    canvas.style.height = `${windowHeight }px`;
-    body.style.height = `${(windowWidth * gameRatio) }px`;
+    canvas.style.width = `${windowHeight * gameRatio}px`;
+    canvas.style.height = `${windowHeight}px`;
+    body.style.height = `${windowWidth * gameRatio}px`;
   }
 }
 
 window.onload = () => {
   resize();
-  window.addEventListener("resize", resize);
+  window.addEventListener('resize', resize);
 };
 
 export default config;
